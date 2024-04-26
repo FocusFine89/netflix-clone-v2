c8ef54925358be86a4c3ca4e2f1861abc6f994bc
FocusFine89 <niivanov02@gmail.com>
2024-04-26 22:29:30 +0200

    change footbar

--- src/components/FootBar.jsx
+++ src/components/FootBar.jsx
@@ -1,50 +1,50 @@
-import { Button } from "react-bootstrap"
+import { Button, Col, Row } from "react-bootstrap"
 
 
 function FootBar(){
 return(
     <footer className="bg-dark text-secondary d-flex flex-column">
-      <div className="links row">
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+      <Row className="links row">
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Audio and Subtitles</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Audio Description</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Help Center</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Gift Cards</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Media Center</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Investor Relations</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Jobs</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Terms of Use</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Privacy</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Legal Notices</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Cookie Preferences</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Corporate Information</p>
-        </div>
-        <div className="col-6 col-sm-4 col-md-3 my-3 ">
+        </Col>
+        <Col xs={6} sm={4} md={3} lg={2} className=" my-3 ">
           <p href="#" className="link-secondary">Contact Us</p>
-        </div>
-      </div>
+        </Col>
+      </Row>
       <div className="d-flex justify-content-center">
         <Button
         type="button"
