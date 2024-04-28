5cc219c8edb697d45fa0ded8d0fa310602feb8a2
FocusFine89 <niivanov02@gmail.com>
2024-04-28 19:43:25 +0200

    change format

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 import FootBar from "./components/FootBar";
-import TopBar from './components/TopBar';
+import TopBar from "./components/TopBar";
 import Gallery from "./components/Gallery";
 import Home from "./components/Home";
 import "slick-carousel/slick/slick.css";
@@ -12,13 +12,13 @@ function App() {
   return (
     <div className="App text-light overflow-x-hidden">
       <header className="App-header">
-        <TopBar/>
-        <Home/>
-        <Gallery titolo="Trending Now" film="Harry Potter"/>
-        <Gallery titolo="Watch it Again" film="Star Wars"/>
-        <Gallery titolo="New Releases" film="Spongebob"/>
-        <ProfilePage/>
-        <FootBar/>
+        <TopBar />
+        <Home />
+        <Gallery titolo="Trending Now" film="Harry Potter" />
+        <Gallery titolo="Watch it Again" film="Star Wars" />
+        <Gallery titolo="New Releases" film="Spongebob" />
+        <ProfilePage />
+        <FootBar />
       </header>
     </div>
   );
